6bee3b8fd9bd3226c7beae87a1e671a2cbef9bf2
eacallam <eacallam@yahoo.com>
2017-08-15 19:35:01 -0400

    First Commit

---
+++ 08.15.17 VariablesOperatorsExpressions/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08._15._17_VariablesOperatorsExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
